e5cf132b24a6d218de41c8d9888689522f0b7cf9
Vinh La <kietla@live.co.uk>
2023-12-17 23:19:27 +0000

    added a new hero image for mobile and changed title location for viewports under 768px

--- my-teeth-aligner-tracker/src/components/hero/hero.js
+++ my-teeth-aligner-tracker/src/components/hero/hero.js
@@ -2,18 +2,16 @@ import React from "react";
 import { Button } from "react-bootstrap";
 import heroImage from "./hero.png"; // Importing the hero image
 import "./hero.css";
+import heroImageMobile from "./hero_mobile.png"; // Mobile image
 
 function HeroSection() {
   return (
-    <div
-      className="hero-section"
-      style={{ backgroundImage: `url(${heroImage})` }}
-    >
+    <div className="hero-section">
       <div className="container">
-        <div className="row align-items-center" style={{ minHeight: "50vh" }}>
+        <div className="row">
           {" "}
           {/* Adjust height as needed */}
-          <div className="col-md-6 ms-3">
+          <div className="col-md-6 hero-title-and-btn">
             <h1 className="hero-title text-black">
               Track your smile journey with ease
             </h1>
